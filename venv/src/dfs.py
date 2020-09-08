a0a2db1f2328e8d2a956fbb6aa458b1ee57c9637
Giovanni <giovanni.alberto@hotmail.com>
2020-09-08 20:08:13 +0100

    trabajo final

---
+++ venv/src/dfs.py
@@ -0,0 +1,59 @@
+from node import Node
+
+def dfs(map, start, end):
+
+    start_node = Node(None, start)
+    end_node = Node(None, end)
+
+    open_list = []
+    closed_list = []
+
+    open_list.append(start_node)
+
+    while len(open_list) > 0:
+
+        current_node = open_list[0]
+        current_index = 0
+
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]
+
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            if node_position[0] > (len(map) - 1) or node_position[0] < 0 or node_position[1] > (len(map[0])) or \
+                    node_position[1] < 0:
+                continue
+
+            if map[node_position[0], node_position[1]] != 0:
+                continue
+
+            new_node = Node(current_node, node_position)
+
+            children.append(new_node)
+
+        for child in reversed(children):
+
+            def inner(closed, actual):
+                for closed_child in closed:
+                    if actual == closed_child:
+                        return True
+
+            if inner(closed_list, child):
+                continue
+
+            open_list.insert(0, child)
+
+    print 'failure'
+
+
+
--- venv/src/main.py
+++ venv/src/main.py
@@ -1,7 +1,11 @@
 import cv2 as cv
 import sys
 from astar import astar
+from dfs import dfs
+from bfs import bfs
 from image_utils import treat_img, draw_rectangle
+import copy
+import time
 
 img = cv.imread('./resources/housemap.pgm')
 
@@ -12,14 +16,44 @@ if img is None:
 treated_img, regular_grid = treat_img(img)
 
 start = (1, 1)
-end = (10, 10)
+print regular_grid[10, 50]
+end = (11, 8)
 
+start_time = time.time()
 path = astar(regular_grid, start, end)
+astar_time = time.time() - start_time
+
+start_time = time.time()
+dfs_path = dfs(regular_grid, start, end)
+dfs_time = time.time() - start_time
+
+start_time = time.time()
+bfs_path = bfs(regular_grid, start, end)
+bfs_time = time.time() - start_time
+
+astar_image = copy.copy(treated_img)
+dfs_image = copy.copy(treated_img)
+bfs_image = copy.copy(treated_img)
 
 for node in path:
-    treated_img = draw_rectangle(treated_img, node[0], node[1])
+    astar_image = draw_rectangle(astar_image, node[0], node[1])
 
-cv.imshow('path', treated_img)
+for node in dfs_path:
+    dfs_image = draw_rectangle(dfs_image, node[0], node[1])
+
+for node in bfs_path:
+    bfs_image = draw_rectangle(bfs_image, node[0], node[1])
+
+cv.imshow('astar_path', astar_image)
+cv.imshow('dfs_path', dfs_image)
+cv.imshow('bfs_path', bfs_image)
 
 cv.waitKey(0)
 
+print(len(path))
+print(len(dfs_path))
+print(len(bfs_path))
+
+print astar_time
+print dfs_time
+print bfs_time
